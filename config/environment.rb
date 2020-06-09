4ebf768c1d287f0820e0fa298943a874ea48cf70
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-09 01:58:18 +0000

    modified folderS

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/Worlds_wealthiest_people/scraper'
+require_relative '../lib/Worlds_wealthiest_people/person'
+require_relative '../lib/Worlds_wealthiest_people/cli'
+require_relative '../lib/Worlds_wealthiest_people/version'
